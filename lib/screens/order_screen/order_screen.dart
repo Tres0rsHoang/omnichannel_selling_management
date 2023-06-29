d20c405b6a6fb36e8f9b27c4e6f79f982d167ac7
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-06-29 11:46:24 +0700

    build: build seach text field and calendar filter icon

--- lib/screens/order_screen/order_screen.dart
+++ lib/screens/order_screen/order_screen.dart
@@ -11,10 +11,100 @@ class OrderScreen extends StatefulWidget {
 class _OrderState extends State<OrderScreen> {
   int _currentIndex = 0;
 
+  void _onHandleScan() {
+    print('Scan');
+  }
+
+  void _onHandleCalendarFilterModal() {
+    print('Calendar');
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 300,
+                    child: TextField(
+                      cursorColor: Colors.grey[500],
+                      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none,
+                        ),
+                        isDense: true,
+                        fillColor: Colors.blueGrey[50],
+                        filled: true,
+                        hintText: 'Nhập để tìm kiếm',
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[500],
+                        ),
+                        prefixIcon: Icon(Icons.search,
+                            color: Colors.grey[500], size: 35),
+                        suffixIcon: InkWell(
+                          onTap: _onHandleScan,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              VerticalDivider(
+                                color: Colors.grey[500],
+                                thickness: 1,
+                                width: 10,
+                                indent: 10,
+                                endIndent: 10,
+                              ),
+                              Icon(Icons.qr_code,
+                                  color: Colors.grey[500], size: 35),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: InkWell(
+                            onTap: _onHandleCalendarFilterModal,
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey[50],
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Icon(Icons.calendar_month_sharp,
+                                  color: Colors.grey[500], size: 35),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(
